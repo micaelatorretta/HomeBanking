ebbd2e8df1e299a0392a18372207268b6a6900f7
micaelatorretta <micaelavtorretta@gmail.com>
2023-08-08 09:59:38 -0300

    Agrega la función de cierre de sesión (signOut)

--- HomeBanking/wwwroot/js/cards.js
+++ HomeBanking/wwwroot/js/cards.js
@@ -11,7 +11,7 @@
             //axios.get("/api/clients/1")
             axios.get("/api/clients/current")
                 .then(function (response) {
-                    //get client ifo
+                    //get client info
                     app.clientInfo = response.data;
                     app.creditCards = app.clientInfo.cards.$values.filter(card => card.type == "CREDIT");
                     app.debitCards = app.clientInfo.cards.$values.filter(card => card.type == "DEBIT");
@@ -19,13 +19,23 @@
                 .catch(function (error) {
                     // handle error
                     app.error = error;
-                })
+                });
         },
         formatDate: function (date) {
             return new Date(date).toLocaleDateString('en-gb');
-        }
+        },
+        signOut: function () {
+            axios.post('/api/auth/logout')
+                .then(response => {
+                    window.location.href = "/index.html";
+                })
+                .catch(error => {
+                    this.error = "Sign out failed";
+                    this.errorToats.show();
+                });
+        },
     },
     mounted: function () {
         this.getData();
     }
-})
+});
